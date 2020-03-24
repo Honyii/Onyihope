e902a58cc3e47f66055e80e184189de921ee97ba
Honyii <amaechihope20@gmail.com>
2020-03-24 15:20:37 +0100

    tasks

---
+++ task3.js
@@ -0,0 +1,11 @@
+let name = "Amaechi";
+let courses= ["Html", "Css", "JavaScript"];
+console.log(name);
+console.log(courses);
+
+
+// for even numbers
+for (i = 0; i <=200; i++){
+    if (( i % 2) == 0)
+        console.log(i);
+}
